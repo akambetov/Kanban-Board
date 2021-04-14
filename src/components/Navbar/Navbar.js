4b48f14be07f63b43493677352b8c8e6a8b960f0
akambetov <almat_kambetov@mail.ru>
2021-04-14 23:57:31 +0600

    Navigation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,60 +4,52 @@ import arrowDown from '../../img/arrow-down.svg';
 import arrowUp from '../../img/arrow-up.svg';
 
 function KanbanNavbar() {
-  let [isOpenDropdown, setIsOpenDropdown] = useState(false);
+  const [isOpenDropdown, setIsOpenDropdown] = useState(() => false);
 
-  function changeArrow() {
-    console.log('click');
+  function toggleMenu(e) {
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    dropdownMenu.classList.toggle('show');
     setIsOpenDropdown((prev) => {
       console.log(isOpenDropdown);
       return !prev;
     });
   }
 
-  // function increment() {
-  //   // setCounter(counter + 1)
-  //   // setCounter(counter + 1)
-  //   setCounter((prevCounter) => {
-  //     return prevCounter + 1;
-  //   });
-  //   // setCounter(prev => prev + 1)
-  // }
-
-  // function decrement() {
-  //   setCounter(counter - 1);
-  // }
-
   return (
     <>
-      {isOpenDropdown}
-      <nav className="navbar-theme navbar navbar-expand-lg">
+      <nav className="navbar-theme navbar">
         <a className="navbar-brand" href="#">
           Awesome Kanban Board
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item dropdown" onClick={() => changeArrow()}>
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
+        <div className="dropdown mr-2" onClick={toggleMenu}>
+          <button className="btn p-0 border-0 outline-none" type="button">
+            <div className="dropdown-userAvatar">
+              <img src={userAvatar} alt="user-avatar" />
+            </div>
+            <img
+              className="dropdown-arrow"
+              src={isOpenDropdown ? arrowUp : arrowDown}
+              alt="arrow-down"
+            />
+          </button>
+          <div
+            className="dropdown-menu dropdown-menu--translate"
+            aria-labelledby="dropdownMenuButton"
+          >
+            <a className="dropdown-item" href="#">
+              Action
+            </a>
+            <a className="dropdown-item" href="#">
+              Another action
+            </a>
+          </div>
+        </div>
+
+        {/* <div className="mr-2" style={{ width: 40 + 'px' }} onClick={toggleMenu}>
+          <ul className="navbar-nav">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#">
                 <div className="dropdown-userAvatar">
                   <img src={userAvatar} alt="user-avatar" />
                 </div>
@@ -67,10 +59,7 @@ function KanbanNavbar() {
                   alt="arrow-down"
                 />
               </a>
-              <div
-                className="dropdown-menu dropdown-menu--translate"
-                aria-labelledby="navbarDropdown"
-              >
+              <div className="dropdown-menu dropdown-menu--translate">
                 <a className="dropdown-item" href="#">
                   Action
                 </a>
@@ -80,7 +69,7 @@ function KanbanNavbar() {
               </div>
             </li>
           </ul>
-        </div>
+        </div> */}
       </nav>
     </>
   );
