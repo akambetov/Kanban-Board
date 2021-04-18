999d20bab5b97be5d22142d204c0e1233852e1fb
akambetov <almat_kambetov@mail.ru>
2021-04-18 14:47:07 +0600

    action for Backlog table adding

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,5 +1,20 @@
 export default function (state, action) {
   switch (action.type) {
+    case 'get-items':
+      console.log(state.backlog.issues);
+      return state.backlog.issues;
+    case 'add-backlog':
+      console.log('reducer');
+      console.log(state);
+      state.backlog.issues = [
+        ...state.backlog.issues,
+        {
+          id: Date.now(),
+          title: action.payload,
+          desc: 'DESCRIPTION FROM REDUCER',
+        },
+      ];
+      return state;
     default:
       return state;
   }
