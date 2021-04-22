6f994f0c0d7821f35f445b998d4d1df8c435b8f8
akambetov <almat_kambetov@mail.ru>
2021-04-23 00:39:06 +0600

    Custom select, boards styles

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Container({ children }) {
   return (
     <div
-      className="container-fluid container-main d-flex justify-content-around h-100"
+      // className="container-fluid container-main d-flex justify-content-around h-100"
+      className="container-main"
       // style={{ height: 94 + 'vh' }}
     >
       {children}
