b1ff60343d0a5e1c1272dc49c803c2a7c644ffaa
akambetov <almat_kambetov@mail.ru>
2021-04-22 22:42:15 +0600

    Fix custom select

--- src/components/CustomSelect.js
+++ src/components/CustomSelect.js
@@ -1,52 +1,66 @@
 import React from 'react';
 
-export default ({updateFrom, issuesType, handleSelectTitle}) => {
-    if (!updateFrom) return null;
-    
-    const domSelectElem = {
-        selectItems: document.querySelector('.custom-select-items'),
-        selectField: document.querySelector(`[data-issue-type=select-field-${issuesType}]`),
-    };
+export default ({ updateFrom, issuesType, handleSelectTitle }) => {
+  if (!updateFrom) return null;
 
-    const handleSelectClick = (e) => {
-        domSelectElem.selectItems.classList.toggle('hide');
-        e.target.classList.toggle('select-icon--down');
-        e.target.classList.toggle('select-icon--up');
-    }
-    const handleItemClick = (e) => {
-        domSelectElem.selectItems.classList.toggle('hide');
-        handleSelectTitle(e.target.value)
-        
-        const taskTitle = updateFrom.issues.find((item) => item.id === e.target.value).title;
-        domSelectElem.selectField.innerText = taskTitle;
+  const domSelectElem = {
+    selectItems: document.querySelector(
+      `[data-select-type=select-items-${issuesType}]`
+    ),
+    selectField: document.querySelector(
+      `[data-issue-type=select-field-${issuesType}]`
+    ),
+  };
 
-        domSelectElem.selectField.classList.toggle('select-icon--down');
-        domSelectElem.selectField.classList.toggle('select-icon--up');
-    }
+  const handleSelectClick = (e) => {
+    domSelectElem.selectItems.classList.toggle('hide');
+    e.target.classList.toggle('select-icon--down');
+    e.target.classList.toggle('select-icon--up');
+  };
+  const handleItemClick = (e) => {
+    domSelectElem.selectItems.classList.toggle('hide');
+    handleSelectTitle(e.target.value);
 
-    return (
-        <>
-        <div className='custom-select-container'>
-            <div className='custom-select-field select-icon--down' onClick={handleSelectClick} data-issue-type={`select-field-${issuesType}`}>
-                Choose the task
-            </div>
-            <div className='custom-select-items hide'>
-                {updateFrom.issues.map((issue) => (
-                    <div className='custom-select-item  ' key={issue.id}>
-                        <label className='select-label'>
-                            {issue.title}
-                            <input className='select-input'
-                                type='radio'
-                                id={`${issue.id}`}
-                                name={updateFrom.title}
-                                onClick={handleItemClick} 
-                                value={issue.id}
-                            />
-                        </label>
-                    </div>
-                ))}
-            </div>
+    const taskTitle = updateFrom.issues.find(
+      (item) => item.id === e.target.value
+    ).title;
+    domSelectElem.selectField.innerText = taskTitle;
+
+    domSelectElem.selectField.classList.toggle('select-icon--down');
+    domSelectElem.selectField.classList.toggle('select-icon--up');
+  };
+
+  return (
+    <>
+      <div className="custom-select-container">
+        <div
+          className="custom-select-field select-icon--down"
+          onClick={handleSelectClick}
+          data-issue-type={`select-field-${issuesType}`}
+        >
+          Choose the task
         </div>
-        </>
-    );
-}
+        <div
+          className="custom-select-items hide"
+          data-select-type={`select-items-${issuesType}`}
+        >
+          {updateFrom.issues.map((issue) => (
+            <div className="custom-select-item " key={issue.id}>
+              <label className="select-label">
+                {issue.title}
+                <input
+                  className="select-input"
+                  type="radio"
+                  id={`${issue.id}`}
+                  name={updateFrom.title}
+                  onClick={handleItemClick}
+                  value={issue.id}
+                />
+              </label>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
