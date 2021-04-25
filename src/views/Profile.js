804b4ad8ab6ead003feb2cb3ff4ab7849bfbdce7
akambetov <almat_kambetov@mail.ru>
2021-04-25 18:11:57 +0600

    Profile fix

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -13,18 +13,21 @@ function Profile () {
           </div>
           <div className='views-info-list'>
             <ul>
-              <li>Date of Birth: 30.07.1990</li>
-              <li>Place of Birth: Almaty (Kazakhstan)</li>
-              <li>Place of Residence: Almaty (Kazakhstan)</li>
+              <li><strong>Date of Birth</strong>: 30.07.1990</li>
+              <li><strong>Place of Birth:</strong> Almaty (Kazakhstan)</li>
+              <li><strong>Place of Residence:</strong> Almaty (Kazakhstan)</li>
               <li>
-                Education
+                <hr />  
+              <strong>Education</strong>
                 <ul>
-                  <li>School: #62, 2008</li>
-                  <li>College: AUPET, 2012</li>
+                  <li><strong>School:</strong> #62, 2008</li>
+                  <li><strong>College:</strong> AUPET, 2012</li>
                 </ul> 
+                <hr />  
               </li>             
-              <li>Work experience: Network engineer, 2013-2021</li>
-              <li>Hobby
+              <li><strong>Work experience</strong>: Network engineer, 2013-2021</li>
+              <hr />  
+              <li><strong>Hobby</strong>
                 <ul>
                   <li>Running</li>
                   <li>Playing guitar</li>
